2e9efa2135eb6c6e5316cb9413114f904f81b802
sucad hassan <sucad1@hotmail.co.uk>
2023-04-25 11:58:29 +0100

    install mui

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <div >
-        <Header/>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home name= {"Sucaad"} title={"web developer"} />}/>
           <Route path="/contact" element={<Contact/>}/>
